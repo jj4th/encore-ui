f537f9dde193c74ef617a20e710e3d30f162b23d
jj4th <john.andrichak@gmail.com>
2017-02-10 10:23:55 -0600

    chore(rxPageObjects): rxAccountInfo to TypeScript

---
+++ utils/rx-page-objects/src/rxAccountInfo.page.ts
@@ -0,0 +1,89 @@
+'use strict';
+
+import {ElementFinder, ElementArrayFinder} from 'protractor';
+import {$, $$, browser, by} from 'protractor';
+import {rxComponentElement, AccessorPromiseString, Promise} from './rxComponent';
+
+/**
+ * @class
+ */
+export class rxAccountInfoBadge extends rxComponentElement {
+    /**
+     * @description The attribute under `ng-src` for a single badge.
+     */
+    getSrc() {
+        return this.getAttribute('ng-src');
+    }
+
+    /**
+     * @description The `data-name` attribute from a single badge.
+     */
+    getName() {
+        return this.getAttribute('data-name');
+    }
+
+    /**
+     * @description Will get the `data-description` attribute from a single badge.
+     */
+    getDescription() {
+        return this.getAttribute('data-description');
+    }
+
+};
+
+/**
+   @class
+ */
+export class rxAccountInfo extends rxComponentElement {
+
+    constructor(rootElement = $('rx-account-info')) {
+        super(rootElement);
+    }
+
+    private get _status() {
+        return this.$('.account-status');
+    }
+
+    get badges() {
+        return this.$$('.account-info-badge img');
+    }
+
+    /**
+     * @description The name of the account.
+     */
+    getName() {
+        return this.element(by.binding('accountName')).getText();
+    }
+
+    /**
+     * @description The account number.
+     */
+    getNumber(){
+        return this.element(by.binding('accountNumber')).getText();
+    }
+
+    /**
+     * @description The account access policy.
+     */
+    getAccessPolicy() {
+        return this.element(by.binding('accountAccessPolicy')).getText();
+    }
+
+    /**
+     * @description The resulting status is lowercased so that it is easy to use with {@link rxAccountInfo.statuses}.
+     */
+    getStatus() {
+        return this._status.getText();
+    }
+
+    /**
+     * @description Parses a class name from the DOM to determine what the status of the account is.
+     * Designed to be easily compared with {@link rxAccountInfo.statusTypes}.
+     */
+    getStatusType() {
+        return this._status.getAttribute('class').then(classNames => {
+            let className = classNames.match(/msg-(\w+)/);
+            return className === null ? 'active' : className[1];
+        });
+    }
+};
---
+++ utils/rx-page-objects/test/rxAccountInfo.midway.ts
@@ -0,0 +1,113 @@
+'use strict';
+
+import {expect} from 'chai';
+import {$} from 'protractor';
+
+import {rxAccountInfo, rxAccountInfoBadge} from '../index';
+
+let notifications = require('../src/rxNotify.page').rxNotify;
+let demoPage = require('../../demo.page');
+
+describe('elements:account', () => {
+    let account: rxAccountInfo;
+
+    before(() => {
+        demoPage.go('#/elements/AccountInfo');
+        account = new rxAccountInfo($('.demo-simple-account rx-account-info'));
+    });
+
+    it('should show element', () => {
+        expect(account.isDisplayed()).to.eventually.be.true;
+    });
+
+    it('should show the account name', () => {
+        expect(account.getName()).to.eventually.equal('Mosso');
+    });
+
+    it('should show the account number', () => {
+        expect(account.getNumber()).to.eventually.equal('12345');
+    });
+
+    it('should show the account access policy', () => {
+        expect(account.getAccessPolicy()).to.eventually.equal('Full');
+    });
+
+    describe('badges', () => {
+        it('should have four badges on the first Account Info box', () => {
+            expect(account.badges.count()).to.eventually.equal(4);
+        });
+
+        it('should have the correct first badge src via byIndex', () => {
+            let src = 'http://mirrors.creativecommons.org/presskit/icons/cc.large.png';
+            let badge = new rxAccountInfoBadge(account.badges.get(0));
+            expect(badge.getSrc()).to.eventually.equal(src);
+        });
+
+        it('should have the right name on the last badge', () => {
+            let badge = new rxAccountInfoBadge(account.badges.last());
+            expect(badge.getName()).to.eventually.equal('Public Domain');
+        });
+
+        it('should have the right description on the last badge', () => {
+            let description = 'Waives as many rights as legally possible, worldwide.';
+            let badge = new rxAccountInfoBadge(account.badges.last());
+            expect(badge.getDescription()).to.eventually.equal(description);
+        });
+    });
+
+    describe('account with warning status type', () => {
+        let warningAccount: rxAccountInfo;
+
+        before(() => {
+            warningAccount = new rxAccountInfo($('.delinquent-account rx-account-info'));
+        });
+
+        it('should have a name', () => {
+            expect(warningAccount.getName()).to.eventually.equal('DelinquentAccount');
+        });
+
+        it('should have a "deliquent" status', () => {
+            expect(warningAccount.getStatus()).to.eventually.equal('Delinquent');
+        });
+
+        it('should have a "warn" type', () => {
+            expect(warningAccount.getStatusType()).to.eventually.equal("warn");
+        });
+
+    });
+
+    describe('account with info status type', () => {
+        let infoAccount: rxAccountInfo;
+
+        before(() => {
+            infoAccount = new rxAccountInfo($('.unverified-account rx-account-info'));
+        });
+
+        it('should have a name', () => {
+            expect(infoAccount.getName()).to.eventually.equal('UnverifiedAccount');
+        });
+
+        it('should have an "unverified" status', () => {
+            expect(infoAccount.getStatus()).to.eventually.equal('Unverified');
+        });
+
+        it('should have an "info" type', () => {
+            expect(infoAccount.getStatusType()).to.eventually.equal('info');
+        });
+
+    });
+
+    describe('error messages', () => {
+
+        it('should show an error notification when it cannot load badges', () => {
+            var errorMessage = 'Error retrieving badges for this account';
+            expect(notifications.byStack('badgeError').isPresent(errorMessage)).to.eventually.be.true;
+        });
+
+        it('should show an error notification when it cannot load account name', () => {
+            var errorMessage = 'Error retrieving account name';
+            expect(notifications.byStack('nameError').isPresent(errorMessage)).to.eventually.be.true;
+        });
+    });
+
+});
