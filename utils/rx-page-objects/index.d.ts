1dcc7b5a4cc38e856fd96943b06855f82ef3e0db
jj4th <john.andrichak@gmail.com>
2017-02-13 07:50:55 -0600

    chore(rxPageObjects): rxActionMenu to TypeScript

--- utils/rx-page-objects/index.d.ts
+++ utils/rx-page-objects/index.d.ts
@@ -1,5 +1,6 @@
 export {rxComponentElement} from './src/rxComponent';
 export {rxAccountInfo, rxAccountInfoBadge} from './src/rxAccountInfo.page';
+export {rxActionMenu, rxAction} from './src/rxActionMenu.page';
 export {rxDatePicker} from './src/rxDatePicker.page';
 
 import {rxDatePicker as rxDatePickerExercise} from './src/rxDatePicker.exercise';
